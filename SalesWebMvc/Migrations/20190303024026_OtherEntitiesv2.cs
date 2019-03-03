c7d591d5d8de8e6dc123d7e86833c60acc2be1cf
Nuno Barros <nunoapbarros@gmail.com>
2019-03-03 02:43:57 +0000

    Other entities and 2nd and 3rd migrations

---
+++ SalesWebMvc/Migrations/20190303024026_OtherEntitiesv2.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class OtherEntitiesv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Seller_SellerId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Sales",
+                table: "Sales");
+
+            migrationBuilder.RenameTable(
+                name: "Sales",
+                newName: "SalesRecord");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sales_SellerId",
+                table: "SalesRecord",
+                newName: "IX_SalesRecord_SellerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SalesRecord",
+                table: "SalesRecord",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SalesRecord",
+                table: "SalesRecord");
+
+            migrationBuilder.RenameTable(
+                name: "SalesRecord",
+                newName: "Sales");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SalesRecord_SellerId",
+                table: "Sales",
+                newName: "IX_Sales_SellerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Sales",
+                table: "Sales",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Seller_SellerId",
+                table: "Sales",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
