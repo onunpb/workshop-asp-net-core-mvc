38afdfcf16501de9c52971b6d9f7f4298892268a
Nuno Barros <nunoapbarros@gmail.com>
2019-03-05 17:14:15 +0000

    SellerFormViewModel and Department select component

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        //Declaração de dependência
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
